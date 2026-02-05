0ce844e0eb5e3be28b0c053ca3e8da1bf85aaaaf
rasmus <rasm274@gmail.com>
2026-02-05 15:18:10 +0100

    first implementation of mandel grid not working with point yet

--- mandelbort.py
+++ mandelbort.py
@@ -3,6 +3,24 @@ Mandelbrot Set Generator
 Author : [ Your Name ]
 Course : Numerical Scientific Computing 2026
 """
+import numpy as np
+
+def compute_mandelbrot():
+    num = 10  # basically resoulation in each direction
+    x_min = -2
+    x_max = 1
+    y_min = -1.5
+    y_max = 1.5
+
+    x = np.linspace(x_min, x_max, num)   # real axis
+    y = np.linspace(y_min,y_max, num)   # imaginary axis
+
+    for i in range(num):
+        for j in range(num):
+            c = x[i] + 1j * y[j]
+            print(c)
+    return c
+
 def mandelbrot_point(c):
     z = 0
     max_iter = 100
@@ -11,10 +29,13 @@ def mandelbrot_point(c):
         if abs(z) > 2:
             return n 
     return max_iter  
-#def compute_mandelbrot(x):
-#pass
+
+
+
 if __name__=="__main__":
     # c = constant, can be entered as real ... + ... j
-    c=0
-    n=mandelbrot_point(c)
-    print("iterations= ", n, " and point", c)
+    #c=0
+    #n=mandelbrot_point(c)  # iteration n
+    c= compute_mandelbrot()
+    print(f"{c=},")
+    # print(f"{n=},")
