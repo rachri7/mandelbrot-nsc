f6c577c2300572ce69cdf7ff91accb6546b21c27
rasmus <rasm274@gmail.com>
2026-02-05 14:05:24 +0100

    Add mandelbort algorithm

---
+++ mandelbort.py
@@ -0,0 +1,19 @@
+"""
+Mandelbrot Set Generator
+Author : [ Your Name ]
+Course : Numerical Scientific Computing 2026
+"""
+def f(x):
+"""
+Example function .
+Parameters
+----------
+x : float
+Input value
+Returns
+-------
+float
+Output value
+"""
+# TODO : Implement the algorithm
+pass
