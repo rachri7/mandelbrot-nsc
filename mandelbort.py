d71392afd4d0bc9f802be79b3f2524f82876639c
rasmus <rasm274@gmail.com>
2026-02-05 15:03:36 +0100

    First working c=0 test for mandel point

--- mandelbort.py
+++ mandelbort.py
@@ -3,20 +3,18 @@ Mandelbrot Set Generator
 Author : [ Your Name ]
 Course : Numerical Scientific Computing 2026
 """
-def f(x):
-"""
-Example function .
-Parameters
-----------
-x : float
-Input value
-Returns
--------
-float
-Output value
-"""
-# TODO : Implement the algorithm
-pass
-
-
-# i like the thingies good nice
+def mandelbrot_point(c):
+    z = 0
+    max_iter = 100
+    for n in range(max_iter):
+        z = z**2 + c
+        if abs(z) > 2:
+            return n 
+    return max_iter  
+#def compute_mandelbrot(x):
+#pass
+if __name__=="__main__":
+    # c = constant, can be entered as real ... + ... j
+    c=0
+    n=mandelbrot_point(c)
+    print("iterations= ", n, " and point", c)
