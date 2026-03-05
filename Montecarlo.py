5a731b0a90b34e7a4fe7fac21031fb9b24b5de29
rasmus <rasm274@gmail.com>
2026-03-05 14:47:04 +0100

    Exercise 2 Lecture 4, Use multiple cores, again we differ in pi value for each as new run, and aalso speed does not increase significantly after 6 workers

--- Montecarlo.py
+++ Montecarlo.py
@@ -2,29 +2,41 @@ import math
 import random
 import time
 import statistics
+import os
+from multiprocessing import Pool
+import psutil
 
-def estimate_pi_serial(num_samples):
+def estimate_pi_chunk(num_samples):
     inside_circle = 0
 
     for _ in range(num_samples):
         x, y = random.random(), random.random()
-
         if x * x + y * y <= 1:
             inside_circle += 1
 
-    return 4 * inside_circle / num_samples
+    return inside_circle
 
+
+def estimate_pi_parallel(num_samples, num_processes=4):
+    samples_per_process = num_samples // num_processes
+    tasks = [samples_per_process] * num_processes
+
+    with Pool(processes=num_processes) as pool:
+        results = pool.map(estimate_pi_chunk, tasks)
+
+    return 4 * sum(results) / num_samples
 
 if __name__ == "__main__":
     num_samples = 10_000_000
-    times = []
+    # set logical = False only physical cores, if True runs with threads
+    for num_proc in range(1, psutil.cpu_count(logical=False) + 1):
+        times = []
 
-    for _ in range(3):
-        t0 = time.perf_counter()
-        pi_estimate = estimate_pi_serial(num_samples)
-        times.append(time.perf_counter() - t0)
+        for _ in range(3):
+            t0 = time.perf_counter()
+            pi_est = estimate_pi_parallel(num_samples, num_proc)
+            times.append(time.perf_counter() - t0)
 
-    t_serial = statistics.median(times)
+        t = statistics.median(times)
 
-    print(f"pi estimate: {pi_estimate:.6f} (error: {abs(pi_estimate - math.pi):.6f})")
-    print(f"Serial time: {t_serial:.3f}s")
+        print(f"{num_proc:2d} workers: {t:.3f}s  pi={pi_est:.6f}")
